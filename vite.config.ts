f00b6ccfb565e5fc0d644d4e74f0f255aaa80c0e
Jessica Benedictus <jessica@combat.nl>
2026-01-06 15:38:01 +0100

    Adding TS and Vite configurations

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      tsConfigPath: "./tsconfig.src.json",
+      outDir: "dist/types",
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    target: "baseline-widely-available",
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      filename: () => "combat-ui.js",
+      cssFileName: "combat-ui",
+    },
+    rollupOptions: {
+      output: {
+        preserveModules: false,
+      },
+    },
+  },
+});
